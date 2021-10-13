3fb1411fab339214230434d5f5c2c2fe58489b40
Fatima Siddique <fatimasidq@gmail.com>
2021-10-13 12:09:27 -0400

    .

--- arrayOperationsAndProperties.js
+++ arrayOperationsAndProperties.js
@@ -6,3 +6,6 @@ console.log("films at index 0: " + films[0]);
 console.log("films at index 1: " + films[1]);
 console.log("films at index 2: " + films[2]);
 console.log("films at index 3: " + films[3]);
+
+films.push("E.T.");
+console.log(director + " is the director of " + films.length + " films");
