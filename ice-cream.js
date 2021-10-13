4902a60532d42ba565c8176cdb297c5c90bd108c
Fatima Siddique <fatimasidq@gmail.com>
2021-10-13 12:21:26 -0400

    .

--- ice-cream.js
+++ ice-cream.js
@@ -1,13 +1,9 @@
-// Do not modify these first two lines
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
-console.log(iceCreamFlavours);
+const vacationDays = 2 + 3;
 
-// Your code below here...
-iceCreamFlavours.push("root beer")
-console.log(iceCreamFlavours);
-
-console.log(iceCreamFlavours[0]);
-
-console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
-
-console.log(iceCreamFlavours.length);
+if (vacationDays <= 5) {
+  console.log("Not enough for a long vacation.");
+} else if (vacationDays === 5) {
+  console.log("We can take a whole week off, not bad.");
+} else {
+  console.log("We're gonna have a great time!");
+}
